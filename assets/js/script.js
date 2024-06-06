a826f2c4780d21d523691046a61b68faeeeb5266
Fokoda799 <feyt2003@gmail.com>
2024-06-07 00:56:06 +0100

    ok

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,3 +20,19 @@ document.addEventListener('DOMContentLoaded', function() {
     window.addEventListener('resize', checkImages);
     checkImages();
 });
+
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("ul");
+
+function openMenu() {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll("#nav-link");
+
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
+function closeMenu() {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}
